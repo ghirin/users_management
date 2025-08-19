2285fc067200139a1292a6f8192f89d5c947d8f4
Yury Ghirin <yury.ghirin@gmail.com>
2025-08-19 12:49:28 +0000

    очищены временные файлы

--- user_management/settings.py
+++ user_management/settings.py
@@ -1,4 +1,4 @@
-# Email settings for Gmail SMTP
+
 import os
 from dotenv import load_dotenv
 
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
-
+# Email settings for Gmail SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
